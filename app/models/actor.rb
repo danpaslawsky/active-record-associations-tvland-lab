0e4e27282e8b7c6117ef63ebf434f09f98f388ca
Dan Paslawsky <danpaslawsky@gmail.com>
2022-03-23 19:42:49 -0700

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name  #returns first and last name of actor
+       "#{self.first_name} #{self.last_name}"
+    end    
+
+    def list_roles #lists all of the characters that actors has alongside the show that character is in
+       
+        self.characters.map do |c| 
+            "#{c.name} - #{c.show.name}"
+        end
+        
+    end   
 end
